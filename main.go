a7d00d119a690a73ca7af11477972338f5a6c14c
wassan128 <wassan.taiko.master@gmail.com>
2018-11-29 23:22:20 +0900

    [add] json2struct

--- main.go
+++ main.go
@@ -11,8 +11,19 @@ const EndPoint = "http://localhost:8000/top-headlines.json"
 
 type News struct {
 	Status string `json:"status"`
+	TotalResults int `json:"totalResults"`
 	Articles []struct {
+		Source struct {
+			Id string `json:"id"`
+			Name string `json:"name"`
+		}
+		Author string `json:"author"`
 		Title string `json:"title"`
+		Description string `json:"description"`
+		Url string `json:"url"`
+		UrlToImage string `json:"urlToImage"`
+		PublishedAt string `json:"publishedAt"`
+		Content string `json:"content"`
 	} `json:"articles"`
 }
 
