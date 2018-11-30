8880c4159196619a570961d8415c24e9c25b88b7
wassan128 <wassan.taiko.master@gmail.com>
2018-11-30 13:21:00 +0900

    [add] became web app powered by gin

--- main.go
+++ main.go
@@ -5,30 +5,37 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 const EndPoint = "http://localhost:8000/top-headlines.json"
 
+type Source struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
+type Article struct {
+	Source      Source `json:"source"`
+	Author      string `json:"author"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Url         string `json:"url"`
+	UrlToImage  string `json:"urlToImage"`
+	PublishedAt string `json:"publishedAt"`
+	Content     string `json:"content"`
+}
 type News struct {
-	Status       string `json:"status"`
-	TotalResults int    `json:"totalResults"`
-	Articles     []struct {
-		Source struct {
-			Id   string `json:"id"`
-			Name string `json:"name"`
-		}
-		Author      string `json:"author"`
-		Title       string `json:"title"`
-		Description string `json:"description"`
-		Url         string `json:"url"`
-		UrlToImage  string `json:"urlToImage"`
-		PublishedAt string `json:"publishedAt"`
-		Content     string `json:"content"`
-	} `json:"articles"`
+	Status       string  `json:"status"`
+	TotalResults int     `json:"totalResults"`
+	Articles     []Article `json:"articles"`
 }
 
-func main() {
-	res, _ := http.Get(EndPoint)
+func getNews() News {
+	res, err := http.Get(EndPoint)
+	if err != nil {
+		fmt.Println("json load error")
+	}
 	defer res.Body.Close()
 
 	var news News
@@ -36,8 +43,20 @@ func main() {
 	if err := json.Unmarshal(data, &news); err != nil {
 		fmt.Println(err)
 	}
-
-	for idx, n := range news.Articles {
-		fmt.Println(idx, n.Title)
-	}
+	return news
 }
+
+func main() {
+	news := getNews()
+	fmt.Println(news.Articles)
+
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"articles": news.Articles,
+		})
+	})
+	router.Run()
+}
+
